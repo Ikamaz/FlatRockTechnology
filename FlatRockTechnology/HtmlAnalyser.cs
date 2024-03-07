c5a0bc3d337e9973e2e298eab6b5bf2dcc3684df
Irakli Mazmishvili <mazmishviliika@gmail.com>
2024-03-07 21:53:55 +0400

    Add project files.

---
+++ FlatRockTechnology/HtmlAnalyser.cs
@@ -0,0 +1,58 @@
+﻿using FlatRockTechnology;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FlatRockTechnology
+{
+    public class HtmlAnalyser
+    {
+        private readonly HtmlDocument _document;
+
+        public HtmlAnalyser(string rawHtml)
+        {
+            _document = new HtmlDocument();
+            _document.LoadHtml(rawHtml);
+        }
+
+        public IEnumerable<Product> ReadProducts()
+        {
+            var nodes = GetProductNodes();
+            return ConvertNodesToProducts(nodes);
+        }
+
+        private HtmlNodeCollection GetProductNodes() =>
+            _document.DocumentNode.SelectNodes("//div[@class='item']");
+
+        private IEnumerable<Product> ConvertNodesToProducts(HtmlNodeCollection nodes)
+        {
+            if (nodes is null) throw new ArgumentNullException($"{nameof(nodes)} can not be null");
+
+            foreach (var node in nodes)
+            {
+                string productName = ExtractName(node.SelectSingleNode(".//h4/a")?.InnerText.Trim());
+                string price = ExtractPrice(node.SelectSingleNode(".//p")?.InnerText);
+                float rating = ExtractRating(node.GetAttributeValue("rating", ""));
+
+                yield return new Product { ProductName = productName, Price = decimal.Parse(price), Rating = rating };
+            }
+        }
+
+        private string ExtractName(string htmlString) => System.Net.WebUtility.HtmlDecode(htmlString);
+
+        private string ExtractPrice(string priceString) => priceString.Split('$')[1].Trim();
+
+        private float ExtractRating(string ratingString)
+        {
+            if (float.TryParse(ratingString, out float rating))
+                return Math.Min(5, rating / 2);
+
+            return 0;
+        }
+    }
+}
+
---
+++ FlatRockTechnology/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlatRockTechnology
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public float Rating { get; set; }
+
+        public override string ToString() => $"ProductName {ProductName}\nPrice {Price}\nRating {Rating}\n";
+    }
+}
